3aafd1592471356c7e989408423ab30ab6792ed1
azzahraadystiarahmah <azzahraadystiarahmah@gmail.com>
2021-09-30 13:13:47 +0700

    pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Biodata diri</h1>";
+        echo "Nama : Az Zahra Adystia Rahmah <br>
+        TTL : Tangerang, 13 Maret 2003 <br> 
+        Alamat : Vila Tangerang Elok <br>
+        Universitas : Universitas Bina Sarana Informatika <br>
+        Prodi : Sistem Informasi <br> 
+        Fakultas : Teknik dan Informatika <br>
+        Hobi : Menggambar dan membaca <br>";
+    }
+}
